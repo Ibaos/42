a57ac8799ee972ae4b46f8628b29be40820fe968
Benjamin Humeau <bhumeau@z1r11p5.42lyon.fr>
2024-10-16 13:50:27 +0200

    finished ex05;06

--- modules_cpp/cpp_module_01_local/ex06/main.cpp
+++ modules_cpp/cpp_module_01_local/ex06/main.cpp
@@ -6,34 +6,37 @@
 /*   By: bhumeau <bhumeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 10:26:37 by bhumeau           #+#    #+#             */
-/*   Updated: 2024/10/16 11:42:48 by bhumeau          ###   ########.fr       */
+/*   Updated: 2024/10/16 13:48:03 by bhumeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int	main()
+int	main(int argc, char **argv)
 {
-	Harl	test;
-	test.complain("DEBUG");
-	test.complain("INFO");
-	test.complain("INFO");
-	test.complain("WARNING");
-	test.complain("WARNING");
-	test.complain("WARNING");
-	test.complain("ERROR");
-	test.complain("ERROR");
-	test.complain("ERROR");
-	test.complain("ERROR");
-	Harl	new_test;
-	test.complain("ERROR");
-	test.complain("NO");
-	test.complain("WARNING");
-	test.complain("YES");
-	test.complain("INFO");
-	test.complain("DEBUG");
-	test.complain("error");
-	test.complain("");
-	test.complain("INFO");
+	if (argc != 2)
+		return (std::cout << "Need 2 arguments!" << std::endl, 1);
+	std::string level =  argv[1];
+	Harl test;
+	int i = 0;
+	std::string table[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	for (i = 0; i < 4; i++)
+		if (!level.compare(table[i]))
+			break;
+	std::cout << i << std::endl;
+	switch (i)
+	{
+		case 0:
+			test.complain("DEBUG");
+		case 1:
+			test.complain("INFO");
+		case 2:
+			test.complain("WARNING");
+		case 3:
+			test.complain("ERROR");
+			break ;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
 	return (0);
 }
